8bfb54c7e8f9fe1c4709eababffde8dbc6ccf9ad
Bryan Thomas <thomas.bryan.m@gmail.com>
2021-04-08 23:37:15 -0700

    update nav bar

--- todd-thomas-painting-app/src/components/NavigationBar/NavigationBar.js
+++ todd-thomas-painting-app/src/components/NavigationBar/NavigationBar.js
@@ -4,18 +4,10 @@ import styled from "styled-components";
 
 const Styles = styled.div`
   .navbar {
-    background-color: #1acaed;
-  }
-  .navbar-brand{
-    color: #efefef;
-    font-size: 2.2em;
-    &:hover {
-      color: black;
-    }
-    font-family: 'Alfa Slab One';
+    background-color: #cdcdcd;
   }
   .navbar-nav .nav-link {
-    color: #efefef;
+    color: black;
     font-size: 1.8em;
     &:hover {
       color: black;
@@ -26,14 +18,22 @@ export const NavigationBar = () => {
   return (
     <Styles>
       <Navbar expand="lg">
-        <Navbar.Brand href="/"><img src="ttp-logo.png" width="80px" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls= "basic-navbar-nav"/>
-        <Navbar.Collapse id = "basic-navbar-nav">
-            <Nav className = "ml-auto">
-                <Nav.Item><Nav.Link href ="/exterior-estimate">Exterior</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href ="/interior-estimate">Interior</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href ="/cabinet-estimate">Cabinet</Nav.Link></Nav.Item>
-            </Nav>
+        <Navbar.Brand href="/">
+          <img alt="TTP LOGO" src="ttp-logo.png" width="80px" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link href="/exterior-estimate">Exterior</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/interior-estimate">Interior</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/cabinet-estimate">Cabinet</Nav.Link>
+            </Nav.Item>
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
     </Styles>

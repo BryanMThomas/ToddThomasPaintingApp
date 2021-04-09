a8aa4515cb93349055d9fcbf70f7de0da1c009d6
Bryan Thomas <thomas.bryan.m@gmail.com>
2021-04-09 00:27:32 -0700

    add exterior form controls

--- todd-thomas-painting-app/src/components/ExteriorEstimate/ExteriorEstimate.js
+++ todd-thomas-painting-app/src/components/ExteriorEstimate/ExteriorEstimate.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Form, Button, Col, Row } from "react-bootstrap";
+import Layout from "../Layout/Layout";
 import styled from "styled-components";
 
 const Styles = styled.div`
@@ -23,22 +25,142 @@ const Styles = styled.div`
   }
 `;
 export const ExteriorEstimate = () => {
-  const [state, setState] = useState({
-    minPrice: 0,
-    maxPrice: "No Max",
-    minBeds: 0,
-    maxBeds: "No Max",
-    minBaths: 0,
-    firstFloorPrimary: false,
-    hvacAgeRange: 10,
-    waterHeaterAgeRange: 10,
-    priceSqftMin: 0,
-    priceSQFtMax: "No Max",
+  //PRICING CONSTS
+  const deluxeRate = 1.15;
+  const ultimateRate = 1.22;
+  const maximumRate = 1.35;
+
+  const [exteriorState, setExteriorState] = useState({
+    clientName: "",
+    clientAddress: "",
+    clientPhone: "",
+    clientEmail: "",
+    homeSqft: -1,
+    deluxePackagePrice: 0,
+    ultimatePackagePrice: 0,
+    maximumPackagePrice: 0,
   });
+  const handleSqftChange = (e) => {
+    let value = e.target.value;
+    //Set Sqft
+    setExteriorState((prevState) => ({
+      ...prevState,
+      ["homeSqft"]: value,
+      ["deluxePackagePrice"]: Math.round(value * deluxeRate),
+      ["ultimatePackagePrice"]: Math.round(value * ultimateRate),
+      ["maximumPackagePrice"]: Math.round(value * maximumRate),
+    }));
+  };
+  const handleStateChange = (e) => {
+    if (e.target.name === "homeSqft") {
+      handleSqftChange(e);
+    } else {
+      const { name, value } = e.target;
+      setExteriorState((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
+  };
+
+  const submit = () => {};
 
   return (
-    <Styles>
-      <div> Exterior Estimate</div>
-    </Styles>
+    <Layout>
+      <Styles>
+        <h1>Exterior Estimate</h1>
+        <br />
+        <Form>
+          <Form.Row>
+            <Col xs="3">
+              <Form.Label>Client Name: </Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Enter Client Name"
+                required
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Col xs="3">
+              <Form.Label>Client Address: </Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Enter Client Address"
+                required
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Col xs="3">
+              <Form.Label>Client Email: </Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="email"
+                placeholder="Enter Client Email"
+                required
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Col xs="3">
+              <Form.Label>Client Phone: </Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Enter Client Phone"
+                required
+              />
+            </Col>
+          </Form.Row>
+          <br />
+          <hr />
+          <Form.Row>
+            <Col xs="3">
+              <Form.Label>Home Sqft: </Form.Label>
+            </Col>
+            <Col xs="3">
+              <Form.Control type="number" placeholder="Enter SQFT" required />
+            </Col>
+          </Form.Row>
+          <br />
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>Deluxe Package</Form.Label>
+              <Form.Control />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Ultimate Package</Form.Label>
+              <Form.Control />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Maximum Package</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Col xs="8">
+              <Form.Label>Additional Notes</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Col>
+          </Form.Row>
+          <br />
+          <Button variant="primary" type="submit">
+            Download &darr;
+          </Button>
+        </Form>
+      </Styles>
+    </Layout>
   );
 };
